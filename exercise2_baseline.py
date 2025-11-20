d300928c9cfe4696d8e346092eba84c1c9c2dcfe
Francesco Foresta <fr.foresta@gmail.com>
2025-11-20 09:44:21 +0100

    Updated Exercise 2

---
+++ exercise2_baseline.py
@@ -0,0 +1,74 @@
+"""
+Exercise 2 Baseline Script
+- Connects to devices using ncclient
+- Stubs for edit-config and get-config operations
+- To be completed by attendees
+"""
+
+# Example XML payload for eth0 IPv4 address (to be used in edit-config)
+EDIT_CONFIG_TEMPLATE = '''
+<config>
+    <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+        <interface>
+            <name>eth0</name>
+            <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
+                <address>
+                    <ip>{ip}</ip>
+                    <netmask>{netmask}</netmask>
+                </address>
+            </ipv4>
+        </interface>
+    </interfaces>
+</config>
+'''
+
+from ncclient import manager
+
+# List of device connection details (to be filled by attendees)
+devices = [
+    # Example:
+    # {"host": "127.0.0.1", "port": 830, "username": "admin", "password": "admin", "eth0": {"ip": "192.168.1.10", "netmask": "255.255.255.0"}},
+]
+
+def connect_device(device):
+    """Connect to a device using ncclient manager."""
+    try:
+        with manager.connect(
+            host=device["host"],
+            port=device.get("port", 830),
+            username=device["username"],
+            password=device["password"],
+            hostkey_verify=False,
+            allow_agent=False,
+            look_for_keys=False,
+            timeout=10
+        ) as m:
+            print(f"Connected to {device['host']}")
+            return m
+    except Exception as e:
+        print(f"Failed to connect to {device['host']}: {e}")
+        return None
+
+def edit_config_stub(m, eth0):
+    """
+    Stub for edit-config operation. To be implemented.
+    eth0 is a dict with 'ip' and 'netmask'.
+    Use EDIT_CONFIG_TEMPLATE.format(ip=eth0.get('ip'), netmask=eth0.get('netmask')) to generate the XML payload.
+    """
+    pass
+
+def get_config_stub(m):
+    """Stub for get-config operation. To be implemented."""
+    pass
+
+def main():
+    for device in devices:
+        m = connect_device(device)
+        if m:
+            # TODO: Implement edit-config and get-config logic
+            eth0 = device.get("eth0", {})
+            edit_config_stub(m, eth0)
+            get_config_stub(m)
+
+if __name__ == "__main__":
+    main()
